593dac979b17710128542e6b3e9727eba923af5e
Samuel Estrella <sammye70@hotmail.com>
2019-12-18 05:06:56 +0000

    Updated clsConexion.cs

--- DAL/clsConexion.cs
+++ DAL/clsConexion.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.sqlClient;
+using System.Windows.Forms;
 
 namespace DAL
 {
     class clsConexion
     {
+        public String ConnectionString;
+        private sqlConnection Connection;
+
+        public void Conectar()
+        {
+           try {
+
+            ConnectionString = "";
+            Connection = new sqlConnection(ConnectionString);
+            Connection.Open();
+            MessagerBox.Show("");
+           }
+           Catch(Exception ex){
+               MessagerBox.Show("", ex.Message);
+           }
+           Finally{
+
+               Desconectar();
+           }
+        }
+
+        public void Desconectar()
+        {
+            Connection.Close();
+
+        }
+
+
     }
 }
